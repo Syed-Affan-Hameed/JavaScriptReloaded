b9559945658fc60706e33a7382c0c61ff6836321
Syed Affan Hameed <syedaffanhameed@gmail.com>
2024-09-20 23:33:29 +0530

    dotandbracketnotations

--- helloworld.js
+++ helloworld.js
@@ -1,2 +1,13 @@
-console.log("Hello world bro!");
-console.log("This is your first commit!")
+//dot notation and bracket notation of accessing elements
+let car={
+    name:"Mclaren",
+    topspeed:"240kmph"
+}
+//use this when we do not know the key fo teh object until runtime
+console.log("Accessing the object values using bracket notation")
+console.log(car["name"]);
+console.log(car["topspeed"]);
+
+console.log("Accessing the object values using dot notation");
+console.log(car.name);
+console.log(car.topspeed);
