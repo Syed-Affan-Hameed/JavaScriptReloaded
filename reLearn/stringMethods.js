786e30284e91cad6c7a41276fb4b099ef4691eeb
Syed-Affan-Hameed <syedaffanhameed@gmail.com>
2025-07-06 10:46:37 +0530

    Add examples of string casing methods in stringMethods.js

--- reLearn/stringMethods.js
+++ reLearn/stringMethods.js
@@ -5,3 +5,13 @@ const modifiedString = exampleString.replace('good', 'bad');
 console.log("Original String:-",exampleString);
 
 console.log("Modified String:-",modifiedString);
+
+const exampleEmail = 'JhonnyBrovo@gmail.com';
+
+console.log(exampleEmail.toUpperCase());
+console.log(exampleEmail.toLowerCase());
+// Better for internationalization
+console.log(exampleEmail.toLocaleLowerCase()); 
+console.log(exampleEmail.toLocaleUpperCase());
+
+
