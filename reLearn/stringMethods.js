1ca7c44286d9349164fea71a26fc26a723a94d6c
Syed-Affan-Hameed <syedaffanhameed@gmail.com>
2025-07-06 09:38:43 +0530

    Add string modification example to demonstrate string replacement in stringMethods.js

---
+++ reLearn/stringMethods.js
@@ -0,0 +1,7 @@
+const exampleString = 'A good sentence';
+
+const modifiedString = exampleString.replace('good', 'bad');
+
+console.log("Original String:-",exampleString);
+
+console.log("Modified String:-",modifiedString);
